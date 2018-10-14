31ce3387477a3a5e1186ce5d2f069e14d029ff00
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-10-14 18:16:40 +0200

    Added Message class

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 '''
 The main module that creates GameServer or GameClient object and run their loop.
 '''
-import shooterroyale as sr
-
-
 def main():
     '''
     The main function that chooses between server and client version.
--- shooterroyale/network.py
+++ shooterroyale/network.py
@@ -4,6 +4,7 @@ network.py
 Module that contains network related classes.
 '''
 import enum
+import pickle
 
 
 class MessageType(enum.Enum):
@@ -13,3 +14,24 @@ class MessageType(enum.Enum):
     SERVER_NEW_PLAYER_INFO = 0
     SERVER_PLAYER_INFO = 1
     CLIENT_PLAYER_INFO = 2
+
+
+class Message:
+    '''
+    Message sent via sockets between server and clients.
+    '''
+    def __init__(self, msg_type, msg_data):
+        self.type = msg_type
+        self.data = msg_data
+
+    def pack(self):
+        '''
+        Dump Message object into binary.
+        '''
+        return pickle.dumps(self)
+
+    def unpack(self):
+        '''
+        Load message binary into Message object.
+        '''
+        return pickle.loads(self)
