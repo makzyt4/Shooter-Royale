c0fa889d530171874aff7341e51e5761c0ed7ad5
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-10-14 17:49:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+The main module that creates GameServer or GameClient object and run their loop.
+'''
+
+def main():
+    '''
+    The main function that chooses between server and client version.
+    '''
+    pass
+
+if __name__ == '__main__':
+    main()
